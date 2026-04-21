782cbb8de4d33cad9b1c3969770aab0eb6cdf987
Mileidy Gonzalez <gildacastro1888@gmail.com>
2026-04-21 11:28:07 -0600

    Uso del spinner en la calculadora
    Signed-off-by: Mileidy Gonzalez <gildacastro1888@gmail.com>

--- app/src/main/java/com/example/miprimeraapp/MainActivity.java
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,55 +13,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-public class MainActivity extends AppCompatActivity {
-    TextView tempVal;
-    Button btn;
-    RadioGroup radioGroup;
-    RadioButton opt;
 
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    TextView tempVal;
+    Spinner spn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         btn = findViewById(R.id.btnCalcular);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                tempVal = findViewById(R.id.textNum1);
+            public void onClick(View v) {
+                tempVal = findViewById(R.id.txtNum1);
                 double num1 = Double.parseDouble(tempVal.getText().toString());
 
-                tempVal = findViewById(R.id.textNum2);
+                tempVal = findViewById(R.id.txtNum2);
                 double num2 = Double.parseDouble(tempVal.getText().toString());
 
-                double respuesta = 0;
-                opt = findViewById(R.id.optSuma);
-                if (opt.isChecked()) {
-                    radioGroup = findViewById(R.id.optOpciones);
-                    if (radioGroup.getCheckedRadioButtonId() == R.id.optSuma) {
+                double respuesta = 0.0;
+
+                spn = findViewById(R.id.spnOpciones);
+                switch (spn.getSelectedItemPosition()){
+                    case 0:
                         respuesta = num1 + num2;
-                    }
-                    opt = findViewById(R.id.optResta);
-                    if (opt.isChecked()) {
-                        if (radioGroup.getCheckedRadioButtonId() == R.id.optResta) {
-                            respuesta = num1 - num2;
-                        }
-                        opt = findViewById(R.id.optMultiplicar);
-                        if (opt.isChecked()) {
-                            if (radioGroup.getCheckedRadioButtonId() == R.id.optMultiplicar) {
-                                respuesta = num1 * num2;
-                            }
-                            opt = findViewById(R.id.optDividir);
-                            if (opt.isChecked()) {
-                                if (radioGroup.getCheckedRadioButtonId() == R.id.optDividir) {
-                                    respuesta = num1 / num2;
-                                }
-                                tempVal = findViewById(R.id.lblRespuesta);
-                                tempVal.setText("Respuesta: " + respuesta);
-                            }
-                        }
-                    }
+                        break;
+                    case 1:
+                        respuesta = num1 - num2;
+                        break;
+                    case 2:
+                        respuesta = num1 * num2;
+                        break;
+                    case 3:
+                        respuesta = num1 / num2;
+                        break;
                 }
+                tempVal = findViewById(R.id.lblRespuesta);
+                tempVal.setText("Respuesta: "+ respuesta);
             }
         });
-    };
+    }
 }
